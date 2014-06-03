9e3b07801ce45106f067f301572c106e10cbf3d3
davidbfrogman <info@davebrownphotography.com>
2014-06-02 19:45:26 -0600

    Added lightdance

--- DaveBrownFashionDesign/Denver-Editorial-Photographer.aspx.designer.cs
+++ DaveBrownFashionDesign/Denver-Editorial-Photographer.aspx.designer.cs
@@ -49,6 +49,15 @@ namespace DaveBrownPhotography {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl divWanderlust;
         
         /// <summary>
+        /// divLightdance control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divLightdance;
+        
+        /// <summary>
         /// divBlogHolder control.
         /// </summary>
         /// <remarks>
